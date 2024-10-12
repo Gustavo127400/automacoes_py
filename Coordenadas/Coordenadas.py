ea94cd2144ecd597bf548879197021bb59fe93c1
Gustavo de Souza Silva <125916967+Gustavo127400@users.noreply.github.com>
2024-10-11 22:30:03 -0300

    Auxiliador em coordenadas. Desenvolvido em ".py"

---
+++ Coordenadas/Coordenadas.py
@@ -0,0 +1,7 @@
+#Código para verificar as coordenadas do mouse 
+
+import time
+import pyautogui
+
+time.sleep(2) # Irei colocar um time de 5s para captar a posição.
+print(pyautogui.position()) # Irá imprimir a posição
